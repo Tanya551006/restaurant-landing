d0ff0517d67ee234838782e1ef01d6c76670e3d4
kkuleshov <kkuleshov92@gmail.com>
2021-01-15 15:49:49 +0200

    book-table

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,2 +1,12 @@
-<header class="header">
-</header>
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+      $('.nav').addClass('active');
+  } else {
+      $('.nav').removeClass('active');
+  }
+});
+
+$('.header__block').slick({
+  slidesToShow: 1,
+  dots: true
+})
